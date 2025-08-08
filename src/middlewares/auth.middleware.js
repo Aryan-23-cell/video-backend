b0948e1eeb509a4053c45ee97a2c76f2a3a93f9e
Aryan Suryawanshi <aryanpunaji20@gmail.com>
2025-08-08 18:32:26 +0530

    bug fixing

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,11 +1,14 @@
-import { ApiError } from "../utils/apiError";
-import { asyncHandler } from "../utils/asyncHandler";
-import { jwt } from "jsonwebtoken";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt  from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        console.log(token);
+        
         if(!token) {
             throw new ApiError(401, "Unauthorized request")
         }
