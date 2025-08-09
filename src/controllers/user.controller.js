3be2b19b5bbde567db7196aef684825fe121c15b
Aryan Suryawanshi <aryanpunaji20@gmail.com>
2025-08-09 22:49:26 +0530

    bug_Fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -255,7 +255,7 @@ const changeCurretPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -265,7 +265,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
@@ -274,7 +274,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
             }
         },
         {new: true}
-    ).select("-password");
+    ).select("-password -refreshToken");
 
     return res
     .status(200)
@@ -302,7 +302,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
             }
         },
         {new :true}
-    ).select("-password")
+    ).select("-password -refreshToken")
 
     return res
     .status(200)
@@ -332,7 +332,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
             }
         },
         {new :true}
-    ).select("-password")
+    ).select("-password -refreshToken")
 
     return res
     .status(200)
